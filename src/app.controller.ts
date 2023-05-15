00232c6f4073e73567f48120284a36942ca4be8f
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-14 23:55:22 -0300

    initial controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,5 +5,24 @@ export class AppController {
 
   @Get("/")
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: "Home page - Online Store"
+    };
+  }
+
+  @Get("/about")
+  @Render('about')
+  about() {
+    let viewData= [];
+    viewData["description"] = "This is an about page ...";
+    viewData["author"] = "Developed by: Diego Kennedy";
+    let data1 = 'About us - Online Store';
+
+    return {
+      title: data1,
+      subtitle: "About Us",
+      viewData: viewData
+    };
+  }
 }
