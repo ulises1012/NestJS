db4644ff1770b76719cce02055920577c4212670
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-19 23:05:49 -0300

    Service product dependency injection

---
+++ src/models/products.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "./product.entity";
+import { Repository } from "typeorm";
+
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>
+  ) {}
+}
