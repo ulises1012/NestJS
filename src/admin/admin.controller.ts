de36452e8facb2fd5e3dd9903d8d06f2170e2afe
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-24 00:36:26 -0300

    simple index method and routes

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Render } from "@nestjs/common";
 
 @Controller('/admin')
 export class AdminController {
 
+  @Get('/')
+  @Render('admin/index')
+  index() {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Admin - Online Store';
+    
+
+    return {
+      viewData:viewData,
+    };
+  }
 }
