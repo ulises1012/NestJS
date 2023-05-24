1e72a65cc9cd74a717a79152a5cdf80f363689ef
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-24 13:08:54 -0300

    admin products controller to render products

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Render } from "@nestjs/common";
 import { ProductsService } from "src/models/products.service";
 
 
 @Controller('/admin/products')
 export class AdminProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get('/')
+  @Render('admin/products/index')
+  async index() {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Admin - Online Store';
+    viewData['products'] = await this.productsService.findAll();
+
+    return {
+      viewData: viewData,
+    }
+  };
 }
