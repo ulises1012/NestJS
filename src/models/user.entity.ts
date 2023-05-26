f5f233fb54af11bc9b5f8442f398cd55c0e0904d
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-25 22:57:51 -0300

    user entity initial

---
+++ src/models/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true})
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  balance: number;
+  
+}
