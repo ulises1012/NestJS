07140616e2abcb771c5465ed618b49db8af68add
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-25 23:29:51 -0300

    method to display a form to create new users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, Redirect, Render } from "@nestjs/common";
 import { UsersService } from "src/models/user.service";
 
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly userService: UsersService) {}
+
+  @Get('/register')
+  @Render('auth/register')
+  register() {
+    const viewData = [];
+    viewData['title'] = 'User Register - Online Store';
+    viewData['subtitle'] = 'User Register';
+
+    return {
+      viewData: viewData,
+    }
+  }
 }
