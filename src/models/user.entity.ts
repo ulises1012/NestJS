1f4131cc6e6965e596930ce9b7b2423baae72e3b
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-25 23:04:18 -0300

    user getters and setters

--- src/models/user.entity.ts
+++ src/models/user.entity.ts
@@ -21,5 +21,52 @@ export class User {
 
   @Column()
   balance: number;
-  
+
+  getId(): number {
+    return this.id;
+  }
+
+  setId(id: number) {
+    this.id = id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  setEmail(email: string) {
+    this.email = email;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+
+  setPassword(password: string) {
+    this.password = password;
+  }
+
+  getRole(): string {
+    return this.role;
+  }
+
+  setRole(role: string) {
+    this.role = role;
+  }
+
+  getBalance(): number {
+    return this.balance;
+  }
+
+  setBalance(balance: number) {
+    this.balance = balance;
+  }
 }
