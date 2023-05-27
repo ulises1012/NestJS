7a9b92b389d580cf830df02b69fe48595ed83bba
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-27 16:07:34 -0300

    fixed method issue

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -21,14 +21,14 @@ export class ProductsController {
   @Get('/:id')
   async show(@Param() params, @Res() response) {
     const product = await this.productsService.findOne(params.id);
-    
+
     if (product === null) {
       return response.redirect('/products');
     }
-    
+
     const viewData = [];
-    viewData['title'] = product.getName + ' - Online Store';
-    viewData['subtitle'] = product.getName + '- Product Information';
+    viewData['title'] = product.getName() + ' - Online Store';
+    viewData['subtitle'] = product.getName() + ' - Product Information';
     viewData['product'] = product;
 
     return response.render('products/show', { viewData: viewData });
