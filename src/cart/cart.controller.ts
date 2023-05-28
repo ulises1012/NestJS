d9084313a49900f44dceed2167757f17b7a2f470
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-28 00:43:33 -0300

    method to index view

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Render, Req } from "@nestjs/common";
+import { Product } from "src/models/product.entity";
 import { ProductsService } from "src/models/products.service";
 
 @Controller('/cart')
 export class cartController {
   constructor(private readonly productService: ProductsService) {}
+
+  @Get('/')
+  @Render('cart/index')
+  async index(@Req() request) {
+    let total = 0;
+    let productsInCart: Product[] = null;
+    const productsInSession = request.session.products;
+
+    if (productsInSession) {
+      productsInCart = await this.productService.findByIds(
+        Object.keys(productsInSession),
+      );
+      total = Product.sumPricesByQuantities(productsInCart, productsInSession);
+    }
+
+    const viewData = [];
+    viewData['title'] = 'Cart - Online Store';
+    viewData['subtitle'] = 'Shopping Cart';
+    viewData['total'] = total;
+    viewData['productsInCart'] = productsInCart;
+
+    return {
+      viewData: viewData,
+    };
+  }
 }
