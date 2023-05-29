e7dbf7018f9c4d292991aa2b1d0e741c35d9c75d
ulises1012 <ulisesjimenezrodriguez012@gmail.com>
2023-05-29 16:44:15 -0300

    added purchase method

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -7,13 +7,22 @@ import {
   Redirect,
   Render,
   Req,
+  Res,
 } from '@nestjs/common';
+import { Item } from 'src/models/item.entity';
+import { Order } from 'src/models/order.entity';
+import { OrdersService } from 'src/models/order.service';
 import { Product } from 'src/models/product.entity';
 import { ProductsService } from 'src/models/products.service';
+import { UsersService } from 'src/models/user.service';
 
 @Controller('/cart')
 export class CartController {
-  constructor(private readonly productService: ProductsService) {}
+  constructor(
+    private readonly productService: ProductsService,
+    private readonly usersService: UsersService,
+    private readonly ordersService: OrdersService,
+  ) {}
 
   @Get('/')
   @Render('cart/index')
@@ -57,5 +66,50 @@ export class CartController {
   @Redirect('/cart')
   delete(@Req() request) {
     request.session.products = null;
+  }
+
+  @Get('/purchase')
+  async purchase(@Req() request, @Res() response) {
+    if (!response.session.user) {
+      return response.redirect('/auth/login');
+    } else if (!request.session.products) {
+      return response.redirect('cart');
+    } else {
+      const user = await this.usersService.findOne(request.session.user.id);
+      const productsInSession = request.session.products;
+      const productsInCart = await this.productService.findByIds(
+        Object.keys(productsInSession),
+      );
+
+      let total = 0;
+      const items: Item[] = [];
+      for (let i = 0; i < productsInCart.length; i++) {
+        const quantity = productsInSession[productsInCart[i].getId()];
+        const item = new Item();
+        item.setQuantity(quantity);
+        item.setPrice(productsInCart[i].getPrice());
+        item.setProduct(productsInCart[i]);
+        items.push(item);
+        total = total + productsInCart[i].getPrice() * quantity;
+      }
+
+      const newOrder = new Order();
+      newOrder.setTotal(total);
+      newOrder.setItems(items);
+      newOrder.setUser(user);
+      const order = await this.ordersService.createOrUpdate(newOrder);
+
+      const newBalance = user.getBalance() - total;
+      await this.usersService.updateBalance(user.getId(), newBalance);
+
+      request.session.products = null;
+
+      const viewData = [];
+      viewData['title'] = 'Purchase - Online Store';
+      viewData['subtitle'] = 'Purchase Status';
+      viewData['orderId'] = order.getId();
+
+      return response.render('cart/purchase', { viewData: viewData});
+    }
   }
 }
